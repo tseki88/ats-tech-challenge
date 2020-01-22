24e092a961c5db0ae8d8d25d18c22a8a38990f80
tseki88 <teruhisaseki@gmail.com>
2020-01-21 21:08:32 -0500

    initial commit, testing utilization of react-csv-reader

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+import CSVReader from 'react-csv-reader';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  // Configuration of parsed csv file
+  papaparseOptions = {
+    header: true //First row becomes header, and all data matrix keys become the header value.
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <CSVReader 
+          onFileLoaded={data => console.log(data)}
+          parserOptions={this.papaparseOptions} />
+      </div>
+    );
+  }
 }
 
 export default App;
